58280b14ecc53bc54fd70156ee0f0c75b14b8013
Alex Grieco <awgrieco@gmail.com>
2024-08-23 14:56:02 -0700

    Being less restrictive with provider constraints

--- versions.tf
+++ versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.53, < 5.0"
+      version = ">= 3.53"
     }
 
     random = {
-      version = "~> 3.4"
+      version = ">= 3.4"
     }
   }
 
